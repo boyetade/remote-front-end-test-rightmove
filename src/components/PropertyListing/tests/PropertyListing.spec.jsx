832da44a617ed8ba0ef73bb86afded73f6824b15
Bola <boyetade@hotmail.com>
2024-09-10 22:25:15 +0100

    Adding property data

--- src/components/PropertyListing/tests/PropertyListing.spec.jsx
+++ src/components/PropertyListing/tests/PropertyListing.spec.jsx
@@ -3,6 +3,19 @@ import { render, screen } from '@testing-library/react';
 import { within } from '@testing-library/dom';
 import PropertyListing from '../PropertyListing';
 
+global.fetch = jest.fn(() => 
+  Promise.resolve({
+    json: () => 
+        Promise.resolve([
+            { id: 1, title: 'Property 1' },
+            { id: 2, title: 'Property 2' },
+            { id: 3, title: 'Property 3' },
+            { id: 4, title: 'Property 4' },
+            { id: 5, title: 'Property 5' },
+        ]),
+  })
+);
+
 describe('PropertyListing', () => {
     it('should render five property cards', async () => {
         render(<PropertyListing />);
